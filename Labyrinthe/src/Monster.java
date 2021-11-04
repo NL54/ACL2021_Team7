07c9e8364a23a1eada0fbbc9d5ee281f15229ecf
unknown <resa.mensuri8@etu.univ-lorraine.fr>
2021-11-04 12:05:45 +0100

    ajout labyrinthe

---
+++ Labyrinthe/src/Monster.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Monster {
+	public int[] position;
+	public int pv=1;
+	public int type;
+	
+	Monster(int[]position,int pv,int type){
+		this.position=position;
+		this.pv=pv;
+		this.type=type;
+	}
+	
+	public int[] getPosition() {
+		return position;
+	}
+	
+	public void deplacer() {
+		position=getPosition();
+		
+		Random ran=new Random();
+		
+		int nxt= ran.nextInt(4);
+		
+		if(nxt==0) {             //le monstre va a gauche
+			position[0]-=1;
+		}
+		if(nxt==1) {			// le monstre va droite
+			position[0]+=1;
+		}
+		if(nxt==2) {
+			position[1]+=1;		//le monstre va en haut
+		}
+		if(nxt==3) {
+			position[1]-=1;
+		}
+	}
+	
+	
+
+}
