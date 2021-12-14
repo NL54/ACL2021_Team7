1116f43e94e86885225ba8e4e05844f03da76745
Nicolas LIM <nicolas-alicia@hotmail.com>
2021-12-14 20:57:24 +0100

    ajout des classes monstre et ghost

--- Jeu_Team7/src/model/Monstre.java
+++ Jeu_Team7/src/model/Monstre.java
@@ -1,41 +1,88 @@
 package model;
-import java.util.*;
-public class Monstre {
-	public int[] position;
-	public int pv=1;
-	public int type;
-	
-	Monstre(int[]position,int pv,int type){
-		this.position=position;
-		this.pv=pv;
-		this.type=type;
-	}
-	
-	public int[] getPosition() {
-		return position;
-	}
-	
-	public void deplacer() {
-		position=getPosition();
-		
-		Random ran=new Random();
-		
-		int nxt= ran.nextInt(4);
-		
-		if(nxt==0) {             //le monstre va a gauche
-			position[0]-=1;
-		}
-		if(nxt==1) {			// le monstre va droite
-			position[0]+=1;
-		}
-		if(nxt==2) {
-			position[1]+=1;		//le monstre va en haut
-		}
-		if(nxt==3) {
-			position[1]-=1;
-		}
-	}
-	
-	
 
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Monstre{
+	private int point_de_vie;
+	private int position_x;
+	private int position_y;
+	public Monstre(int point_de_vie, int position_x, int position_y) {
+		this.point_de_vie = point_de_vie;
+		this.position_x = position_x;
+		this.position_y = position_y;
+	}
+	public boolean testVivant() {
+		return (point_de_vie > 0);
+	}
+	public void subirDegat() {
+		this.point_de_vie = Math.max(this.point_de_vie - 1, 0);
+	}
+	public int[] deplacementAleatoire(Labyrinthe L) {
+		boolean deplac;
+		int[] coord = new int[2];
+		int[] nouvellePosition = new int[2];
+		ArrayList<int[]> deplacement_possib= new ArrayList<int[]>();
+		ArrayList<int[]> deplacements = new ArrayList<int[]>();
+		int[] gauche = new int[2];
+		gauche[0] = -1;
+		gauche[1] = 0;
+		int[] droite = new int[2];
+		droite[0] = 1;
+		droite[1] = 0;
+		int[] haut = new int[2];
+		haut[0] = 0;
+		haut[1] = -1;
+		int[] bas = new int[2];
+		bas[0] = 0;
+		bas[1] = 1;
+		deplacements.add(gauche);
+		deplacements.add(droite);
+		deplacements.add(haut);
+		deplacements.add(bas);
+		for (int[] deplacement : deplacements) {
+			int[] coordonnee = new int[2];
+			coordonnee[0] = this.position_x + deplacement[0];
+			coordonnee[1] = this.position_y + deplacement[1];
+			if (this.test_deplacement(L, coordonnee[0], coordonnee[1])) {
+				deplacement_possib.add(coordonnee);
+				}
+		}
+		int taille=deplacement_possib.size();
+		if (taille == 0) {
+			nouvellePosition[0] = this.position_x;
+			nouvellePosition[1] = this.position_y;
+			return nouvellePosition;
+		}
+		else {
+			Random rand= new Random();
+			int[] move = deplacement_possib.get(rand.nextInt(taille));
+			return move;
+		}	
+	}
+	public boolean test_deplacement(Labyrinthe L, int x, int y) {
+		if (!(((Math.abs(x-position_x)==1) && (Math.abs(y-position_y)==0)) || 
+				((Math.abs(x-position_x)==0) && (Math.abs(y-position_y)==1)))) {
+			return false;	
+		}
+		else if (x>=L.longueur || y>=L.largeur || x < 0 || y < 0) {
+			return false;
+		} 
+		else if (this.image.equalsIgnoreCase("ghost")) {
+			if (L.laby[y][x].testPresenceHero())
+				return true;
+			else
+				return !(L.laby[y][x].testPresence());
+		}
+		else {
+			if (L.laby[y][x].testPresenceHero())
+				return true;
+			else if (L.laby[y][x] == 10 && L.laby[y][x] == 1)
+				return false;
+			else
+				return !(L.laby[y][x].testPresence());
+		}
+	}
+}
 }
