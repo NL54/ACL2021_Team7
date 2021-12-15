1f4e8402445a5de503e873362ba6ed69178128f2
LucasMPF1 <92717158+LucasMPF1@users.noreply.github.com>
2021-12-15 14:22:22 +0100

    Update Heros.java

--- Jeu_Team7/src/model/Heros.java
+++ Jeu_Team7/src/model/Heros.java
@@ -7,9 +7,11 @@ import engine.Cmd;
 public class Heros  {
 	int[]pos;
 	public int Hp = 3;
+	this.image = "Heros"
 	
 	Heros( int Hp ){
 		this.Hp = Hp;
+		this.image = "Heros"
 	}
 	public void perdpointdevie(int n) {
 		this.Hp=this.Hp-n;
