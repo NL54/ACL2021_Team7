df2a8f05f5697b9f53c65321e28896aa5378d445
unknown <resa.mensuri8@etu.univ-lorraine.fr>
2022-01-10 02:28:22 +0100

    modif test et ajut

--- Jeu_Team7/src/Test/JeuTest.java
+++ Jeu_Team7/src/Test/JeuTest.java
@@ -9,13 +9,16 @@ import engine.Cmd;
 import model.ErreurHeros;
 import model.Jeu;
 import model.Monstre;
+import model.ghost;
 
 class JeuTest {
 
+
 	private int niveau;
 	private Jeu jeu;
-	Monstre M = new Monstre(1,8,14);
+	Monstre M = new Monstre(1,8,14);// avec [8,14] [x,y] position initiale du monstre
 	private Jeu jeu2;
+	private ghost g= new ghost(1,6,3);
 	@BeforeEach
 	void setUp()  {
 		niveau=1;
@@ -23,6 +26,8 @@ class JeuTest {
 		this.jeu2=new Jeu("helpFilePacman.txt",2);
 		this.jeu2.p1.plateau[1][1]=0;
 		this.jeu.p1f.plateau[1][1]=0;
+		Monstre M = new Monstre(1,8,14);
+		ghost g = new ghost(1,6,3);
 	}
 	
 	@Test
@@ -40,29 +45,33 @@ class JeuTest {
 	}
 	//Test Tresor
 	@Test
-	void testTresorhaut() throws ErreurHeros { //postresor [17,1][x,y]
-		jeu.p1f.plateau[2][17]=2; //ici [y][x]
+	void testTresorhaut() throws ErreurHeros {
+		jeu.p1f.plateau[4][17]=5; //ici [y][x] postresor
+		jeu.p1f.plateau[5][17]=2; // position heros
 		jeu.Tresor(niveau,Cmd.UP);
 		assertTrue(jeu.Tresor(niveau,Cmd.UP));
 		
 	}
 	@Test
 	void testTresorbas() throws ErreurHeros { //postresor [17,1][x,y]
-		jeu.p1f.plateau[0][17]=2;
+		jeu.p1f.plateau[4][17]=5; //ici [y][x] postresor
+		jeu.p1f.plateau[3][17]=2; // position heros
 		jeu.Tresor(niveau,Cmd.DOWN);
 		assertTrue(jeu.Tresor(niveau,Cmd.DOWN));
 		
 	}
 	@Test
 	void testTresorgauche() throws ErreurHeros { //postresor [17,1][x,y]
-		jeu.p1f.plateau[1][18]=2;
+		jeu.p1f.plateau[4][17]=5; //ici [y][x] postresor
+		jeu.p1f.plateau[4][18]=2; // position heros
 		jeu.Tresor(niveau,Cmd.LEFT);
 		assertTrue(jeu.Tresor(niveau,Cmd.LEFT));
 		
 	}
 	@Test
 	void testTresordroit() throws ErreurHeros { //postresor [17,1][x,y]
-		jeu.p1f.plateau[1][16]=2;
+		jeu.p1f.plateau[4][17]=5; //ici [y][x] postresor
+		jeu.p1f.plateau[4][16]=2; // position heros
 		jeu.Tresor(niveau,Cmd.RIGHT);
 		assertTrue(jeu.Tresor(niveau,Cmd.RIGHT));
 		
@@ -72,28 +81,32 @@ class JeuTest {
 	
 	@Test
 	void testactiveMagieHaut() throws ErreurHeros { //postresor [10,11][x,y]
-		jeu.p1f.plateau[12][10]=2;
+		jeu.p1f.plateau[5][11]=4; //pos magie
+		jeu.p1f.plateau[6][11]=2;
 		jeu.activeMagie(niveau,Cmd.UP);
 		assertTrue(jeu.activeMagie(niveau,Cmd.UP));
 		
 	}
 	@Test
 	void testactiveMagieBas() throws ErreurHeros { //postresor [10,11][x,y]
-		jeu.p1f.plateau[10][10]=2;
+		jeu.p1f.plateau[5][11]=4; //pos magie
+		jeu.p1f.plateau[4][11]=2;
 		jeu.activeMagie(niveau,Cmd.DOWN);
 		assertTrue(jeu.activeMagie(niveau,Cmd.DOWN));
 		
 	}
 	@Test
 	void testactiveMagieDroit() throws ErreurHeros { //postresor [10,11][x,y]
-		jeu.p1f.plateau[11][9]=2;
+		jeu.p1f.plateau[5][11]=4; //pos magie
+		jeu.p1f.plateau[5][10]=2;
 		jeu.activeMagie(niveau,Cmd.RIGHT);
 		assertTrue(jeu.activeMagie(niveau,Cmd.RIGHT));
 		
 	}
 	@Test
 	void testactiveMagieGauche() throws ErreurHeros { //postresor [10,11][x,y]
-		jeu.p1f.plateau[11][11]=2;
+		jeu.p1f.plateau[5][11]=4; //pos magie
+		jeu.p1f.plateau[5][12]=2;
 		jeu.activeMagie(niveau,Cmd.LEFT);
 		assertTrue(jeu.activeMagie(niveau,Cmd.LEFT));
 		
@@ -102,70 +115,98 @@ class JeuTest {
 	//Test activepiege
 	@Test
 	void testactivePiegeHaut() throws ErreurHeros { //pos PIEGE [14,14][x,y]
-		jeu.p1f.plateau[15][14]=2;
+		jeu.p1f.plateau[9][13]=3; //position piege
+		jeu.p1f.plateau[10][13]=2; //positio heros
 		jeu.activePiege(niveau, Cmd.UP);
 		assertTrue(jeu.activePiege(niveau, Cmd.UP));
 	}
 	@Test
 	void testactivePiegeBas()throws ErreurHeros { //posPIEGE [14,14][x,y]
-		jeu.p1f.plateau[13][14]=2;
+		jeu.p1f.plateau[9][13]=3; //position piege
+		jeu.p1f.plateau[8][13]=2; //positio heros
 		jeu.activePiege(niveau, Cmd.DOWN);
 		assertTrue(jeu.activePiege(niveau, Cmd.DOWN));
 	}
 	@Test
 	void testactivePiegeDroit()throws ErreurHeros{ //POSPIEGE [14,14][x,y]
-		jeu.p1f.plateau[14][13]=2;
+		jeu.p1f.plateau[9][13]=3; //position piege
+		jeu.p1f.plateau[9][12]=2; //positio heros
 		jeu.activePiege(niveau, Cmd.RIGHT);
 		assertTrue(jeu.activePiege(niveau, Cmd.RIGHT));
 	}
 	@Test
 	void testactivePiegeGauche()throws ErreurHeros { //POSPIEGE [14,14][x,y]
-		jeu.p1f.plateau[14][15]=2;
+		jeu.p1f.plateau[9][13]=3; //position piege
+		jeu.p1f.plateau[9][14]=2; //positio heros
 		jeu.activePiege(niveau, Cmd.LEFT);
 		assertTrue(jeu.activePiege(niveau, Cmd.LEFT));
 	}
 	//test activePassage
 	@Test
 	void testactivePassageBas() throws ErreurHeros{ //POS passage [4 ,12][x,y]
-		jeu2.p1.plateau[11][4]=2;
+		jeu2.p1.plateau[10][4]=6; // pos passage
+		jeu2.p1.plateau[9][4]=2;
 		jeu2.activePassage(niveau, Cmd.DOWN);
 		assertTrue(jeu2.activePassage(niveau, Cmd.DOWN));
 	}
 	
 	@Test
 	void testactivePassageHaut() throws ErreurHeros{ //POS passage [4 ,12][x,y]
-		jeu2.p1.plateau[13][4]=2;
+		jeu2.p1.plateau[10][4]=6; // pos passage
+		jeu2.p1.plateau[11][4]=2;
 		jeu2.activePassage(niveau, Cmd.UP);
 		assertTrue(jeu2.activePassage(niveau, Cmd.UP));
 	}
 	@Test
 	void testactivePassageDroit()throws ErreurHeros { //POS passage [4 ,12][x,y]
-		jeu2.p1.plateau[12][3]=2;
+		jeu2.p1.plateau[10][4]=6; // pos passage
+		jeu2.p1.plateau[10][3]=2;
 		jeu2.activePassage(niveau, Cmd.RIGHT);
 		assertTrue(jeu2.activePassage(niveau, Cmd.RIGHT));
 	}
 	@Test
 	void testactivePassageGauche()throws ErreurHeros { //POS passage [4 ,12][x,y]
-		jeu2.p1.plateau[12][5]=2;
+		jeu2.p1.plateau[10][4]=6; // pos passage
+		jeu2.p1.plateau[10][5]=2;
 		jeu2.activePassage(niveau, Cmd.LEFT);
 		assertTrue(jeu2.activePassage(niveau, Cmd.LEFT));
 	}
 	
 	//Test deplacerMonstre
 	@Test
-	void testDeplacerMonstre() { //le monstre M a la position [8,14][y,x]
+	void testDeplacerMonstre() { //le monstre M a la position [8,14][x,y]
 		jeu.deplace_monstre(M);
 		boolean test = false;
-		if(jeu.p1.plateau[8][15]==7) { // si le monstres'est deplacé à droite
+		if(jeu.p1.plateau[14][9]==7) { // si le monstres'est deplacé à droite
 			test=true;
 		}
-		else if(jeu.p1.plateau[8][13]==7) { // si le monstres'est deplacé à gauche
+		else if(jeu.p1.plateau[14][7]==7) { // si le monstres'est deplacé à gauche
 			test=true;
 		}
-		else if(jeu.p1.plateau[9][14]==7) { // si le monstres'est deplacé en bas
+		else if(jeu.p1.plateau[15][8]==7) { // si le monstres'est deplacé en bas
 			test=true;
 		}
-		else if(jeu.p1.plateau[7][14]==7) { // si le monstres'est deplacé en haut
+		else if(jeu.p1.plateau[13][8]==7) { // si le monstres'est deplacé en haut
+			test=true;
+		}
+		assertTrue(test);
+		
+	}
+	//deplaceghost
+	@Test
+	void testDeplacerGhost() { //le ghost g a la position [6,3][x,y]
+		jeu.deplace_ghost(g);
+		boolean test = false;
+		if(jeu.p1.plateau[3][7]==8) { // si le ghost s'est deplacé à droite
+			test=true;
+		}
+		else if(jeu.p1.plateau[3][5]==8) { // si le ghost s'est deplacé à gauche
+			test=true;
+		}
+		else if(jeu.p1.plateau[4][6]==8) { // si le ghost s'est deplacé en bas
+			test=true;
+		}
+		else if(jeu.p1.plateau[2][6]==8) { // si le ghost s'est deplacé en haut
 			test=true;
 		}
 		assertTrue(test);
